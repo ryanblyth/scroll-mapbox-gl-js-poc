cce96e63d7eb8d7aad120a6220883e7e065e9958
ryanblyth <ryanjblyth@gmail.com>
2025-08-14 16:48:32 -0600

    feat: add initial base html, css, and js structure.

---
+++ js/map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaW5kZWxpYmUiLCJhIjoiY2xvM2k1Z25jMGZmbjJsbW9iMGV0M293cyJ9.UPel041iNYR3w_gq01-X8g';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v11',
+  center: [-105.5, 39.1],
+  zoom: 6
+});
